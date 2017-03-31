e80ce84058f663e362def370f2b65e023e4cb63f
CrazyAndy <auden.sh@163.com>
2017-03-31 18:36:46 +0800

    pv

--- static/js/count.js
+++ static/js/count.js
@@ -16,16 +16,14 @@ $(document).ready(function() {
             console.log('fail');
         }
     });
+	var sendData = {"site_id":"31d4d3da408942182f35606f94a67f3d","method":"source/all/a","metrics":"pv_count","start_date":"20170301","end_date":"20170901"};
 	$.ajax({
             type: 'post',
             url: 'https://api.baidu.com/json/tongji/v1/ReportService/getData',
             cache: false,
-            data: {"site_id":"31d4d3da408942182f35606f94a67f3d",
-					"method":"source/all/a",
-					"metrics":"pv_count",
-					"start_date":"20170301",
-					"end_date":"20170901"},
-            dataType: 'json',
+            data: JSON.stringify(sendData),
+        	contentType: "application/json",
+        	dataType:'json',
             success: function (data) {
                 $("#jalpc_site_pv").html(data);
             }
